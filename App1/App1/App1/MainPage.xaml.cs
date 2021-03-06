ded264d3b1f3cbe1f71c38b393726b1f25b985a0
OscarEstidCorado <ocoradoc1@miumg.edu.gt>
2021-03-05 22:47:05 -0600

    Examen Parcial No.1

---
+++ App1/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            var num = (Dijito.Text);
+
+            num = num.Remove(num.Length - 1, 1);
+
+            var nume = Int32.Parse(num);
+
+            nume = nume - 4;
+
+            Resultado.Text = num.ToString();
+        }
+    }
+}
---
+++ App2 Oscar Corado/App2 Oscar Corado/App2 Oscar Corado/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2_Oscar_Corado
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var numer = (Resultado.Text);
+            var num = Int32.Parse(numer);
+            num = num - 365;
+            var cadena = num.ToString();
+
+            x.Text = cadena.Substring(0, 1);
+            y.Tetx = cadena.Substring(cadena.Length - 2, 2);
+
+
+        }
+    }
+}
